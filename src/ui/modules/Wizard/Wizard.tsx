b908f531d4813f937761c5aae79c0c4936129988
Natacha Ivannikova <natacha.im@gmail.com>
2021-04-15 10:33:08 +0200

    💄 - WizardSteps Styles

--- src/ui/modules/Wizard/Wizard.tsx
+++ src/ui/modules/Wizard/Wizard.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { WizardProvider } from './context/WizardProvider'
 import { WizardContent } from './components/WizardContent';
 import { WizardSteps } from './components/WizardSteps'
+import './Wizard.css'
 
 const Wizard: React.FC = () => {
   return (
-    <>
-      <WizardSteps/>
-      <WizardContent/>
-    </>
+    <section className="Wizard">
+      <WizardProvider>
+        <WizardSteps/>
+        <WizardContent/>
+      </WizardProvider>
+    </section>
   );
 }
 
--- src/ui/modules/Wizard/components/WizardSteps/WizardSteps.tsx
+++ src/ui/modules/Wizard/components/WizardSteps/WizardSteps.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { WizardContext } from '../../context/WizardProvider'
 import './WizardSteps.css';
 
 const WizardSteps: React.FC = () => {
+  const {state} = useContext(WizardContext);
+  console.log(state)
+
   return (
-    <p>WizardSteps</p>
+    <ul className="WizardSteps">
+      <li className="checked"><span>1</span></li>
+      <li className="active"><span>2</span></li>
+      <li><span>3</span></li>
+    </ul>
   );
 }
 
