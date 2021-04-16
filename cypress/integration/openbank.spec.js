4d06ad5e4fb9e0e5b560b4812cff70179de8f45b
Natacha Ivannikova <natacha.im@gmail.com>
2021-04-16 12:02:20 +0200

    ✅ - Added Testing Library and Cypress Tests

--- cypress/integration/openbank.spec.js
+++ cypress/integration/openbank.spec.js
@@ -4,8 +4,22 @@
 context('Openbank', () => {
 
     it('Can open the app', () => {
-
         cy.visit('http://localhost:3000/');
+    })
+    it('Can the button will be disabled', () => {
+        const button = cy.get('.actions button')
+        button.should('be.disabled')
+    })
+    it('Can be enabled if you press the check', () => {
 
+        const checkbox = cy.get('#thermsOfAge')
+        checkbox.check()
+        const button = cy.get('.actions button')
+        button.should('not.be.disabled')
+    })
+    it('You can see the text on the screen if you have the check pressed and press next', () => {
+        const button = cy.get('.actions button')
+        button.click();
+        cy.contains('Crea tu Contraseña Maestra');
     })
 });
