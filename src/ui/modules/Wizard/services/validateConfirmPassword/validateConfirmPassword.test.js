e8aa77b3f5598c7c454de06d7b18730fcdf6ff65
Natacha Ivannikova <natacha.im@gmail.com>
2021-04-16 09:58:08 +0200

    ✅ - Added unit test

--- src/ui/modules/Wizard/services/validateConfirmPassword/validateConfirmPassword.test.js
+++ src/ui/modules/Wizard/services/validateConfirmPassword/validateConfirmPassword.test.js
@@ -15,4 +15,18 @@ describe('validateConfirmPassword', () => {
             error: true
         })
     })
+
+    it('return false when passwords match', () => {
+        // Arrange
+        const passWord = '123';
+        const confirmPassword = '123';
+
+        // Act
+        const value = validateConfirmPassword(confirmPassword, passWord);
+
+        // Assert 
+        expect(value).toEqual({
+            error: false
+        })
+    })
 })
